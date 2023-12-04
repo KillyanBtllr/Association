5e401e279ab001d71b2d77b9f83c3995e71f94eb
KillyanBtllr <batailler.killyan@gmail.com>
2023-12-04 13:21:47 +0100

    Update accueil

---
+++ view/admin/activite.php
@@ -0,0 +1,68 @@
+<?php 
+$sqlSelect = "SELECT prenom_participant FROM participant JOIN utilisateur ON participant.num_participant = utilisateur.num_participant WHERE id_user = :id_user";
+$stmtSelect = $pdo->prepare($sqlSelect);
+
+$id_user = $_SESSION["id_user"];
+
+$stmtSelect->bindParam(':id_user', $id_user);
+
+$stmtSelect->execute();
+
+$infoUser = $stmtSelect->fetch();
+
+//Récup l'id user
+$prenom = $infoUser['prenom_participant'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
+    <link href="css/accueil.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.2/angular.min.js"></script>
+    <script src="js/accueil.js"></script>
+</head>
+<body>
+    <header>
+        <div class="in-header">
+            <span class="headBase-texte"><a href="/5bear/lab/index.php" class="lien-head">Association</a></span>
+        </div>
+        <span class="head-base-menu"></span>
+        <div class="membre__menu">
+            <div class="membre__etage-first">
+                <img class="icone" src="img/utilisateur.svg">
+                <p class="membre__infos-pseudo">Administrateur</p>
+                <i class="material-icons membre__infos-expand">expand_more</i>
+            </div>
+            <div class="membre__etage-menu">
+                <a href="" class="membre__link"><i class="material-icons membre__link-icons">account_circle</i> Mon Compte</a>
+                <a href="deconnexion.php" class="membre__link"><i class="material-icons membre__link-icons">exit_to_app</i> Déconnexion</a>
+            </div>
+        </div>
+    </header>
+    <div class="menu">
+        <div class="menubutton">
+            <div class="blok een"></div>
+            <div class="blok twee"></div>
+            <div class="blok drie"></div>
+        </div>
+        <ul class="">
+            <li class="accueil"><div class="menutekstwrapper"><a class="menutekst" href="">Accueil</a></div></li>
+            <li class="activite"><div class="menutekstwrapper"><a class="menutekst" href="">Activités</a></div></li>
+            <li class="creneau"><div class="menutekstwrapper"><a class="menutekst" href="">Créneaux</a></div></li>
+            <li class="utilisateur"><div class="menutekstwrapper"><a class="menutekst" href="">Responsables</a></div></li>
+            <li class="utilisateur"><div class="menutekstwrapper"><a class="menutekst" href="">Participants</a></div></li>
+            <li class="utilisateur"><div class="menutekstwrapper"><a class="menutekst" href="">Utilisateurs</a></div></li>
+        </ul>
+    </div>
+    <section class="">
+        <h1>Bienvenue <?php echo $prenom; ?></h1>
+    </section>
+    
+
+</body>
+</html>
